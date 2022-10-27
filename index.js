7a059b5ad7d16feaaad1a2e79d754885dbaabc0a
nikita morev <morevnsw@gmail.com>
2022-10-27 10:11:08 +0300

    initial

---
+++ index.js
@@ -0,0 +1,14 @@
+/**
+ * @description - trigger callback on user's move
+ * @param {function} callback
+ */
+export function onUserAction(callback) {
+    const documentEvents = ['touchmove', 'click', 'keydown', 'mousemove']
+
+    function handler() {
+        documentEvents.forEach(event => document.body.removeEventListener(event, handler))
+        callback();
+    }
+
+    documentEvents.forEach(event => document.body.addEventListener(event, handler))
+}
